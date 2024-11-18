f7b00361762232b33915603b8580f502147bba22
callmeahha <61609083+callmeahha@users.noreply.github.com>
2024-11-18 23:32:54 +0900

    dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -7,10 +7,6 @@ from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
 
 
-font_path = './NanumGothic.ttf'  # 프로젝트 폴더에 폰트 파일 직접 포함
-font = font_manager.FontProperties(fname=font_path)
-plt.rcParams['font.family'] = font.get_name()
-
 
 st.set_page_config(
     page_title="편의점 영양성분 분석",
